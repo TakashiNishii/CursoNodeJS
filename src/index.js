0d9859f87b1c9d0bb0cda7421ff22bf32dbdfd64
Takashi <ygortn14@gmail.com>
2021-02-08 18:22:11 -0400

    Aula 2 - Autenticacão

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000);
