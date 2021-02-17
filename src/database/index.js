38768b0c2421cfb353830d93a7303254930fc1c4
Takashi <ygortn14@gmail.com>
2021-02-17 17:51:15 -0400

    Aula4- CRUD e relacionamentos com MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true, useFindAndModify: false,useUnifiedTopology: true, useCreateIndex: true});
 
 
 module.exports = mongoose;
